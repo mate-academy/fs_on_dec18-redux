aeda1d62315ae8ed067be658ee83d8ae25a69364
mgrinko <glukym@gmail.com>
2019-03-28 10:07:51 +0200

    Simple reducer implementation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import { createStore } from 'redux';
+import React from 'react';
 import { connect } from 'react-redux';
+// const API_URL = 'https://mate-academy.github.io/fe-program/js/tasks/people/people.json';
 
-const API_URL = 'https://mate-academy.github.io/fe-program/js/tasks/people/people.json';
 
 
-const initialState = {
-  people: [],
-  isLoaded: false,
-};
+const App = ({ count }) => (
+  <h1>count: {count}</h1>
+);
 
-const rootReducer = (state, action) => state;
-const store = createStore(rootReducer, initialState);
+const mapStateToProps = (state) => ({
+  count: state.people.length,
+});
 
-console.log(store.getState());
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Redux</h1>
-      </div>
-    );
-  }
-}
+const ConnectedApp = connect(
+  mapStateToProps
+)(App);
 
-export default App;
+export default ConnectedApp;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
 import App from './App';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+  people: [],
+  isLoaded: false,
+};
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_PERSON':
+      return {
+        ...state,
+        people: [...state.people, action.person],
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(rootReducer, initialState);
+
+
+store.dispatch({
+  type: 'ADD_PERSON',
+  person: { id: 1 },
+});
+
+console.log(
+  store.getState()
+);
+
+
+
+
+
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
