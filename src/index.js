3dd8c94640d5cb4a9386efd59d9ef644e4f43d94
mgrinko <glukym@gmail.com>
2019-03-28 11:56:08 +0200

    think was added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+
 
 import App from './App';
 
@@ -23,17 +25,19 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(),
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
 );
 
-const oldDispatch = store.dispatch;
-store.dispatch = (action) => {
-  if (typeof action === 'function') {
-    action(oldDispatch);
-  } else {
-    oldDispatch(action);
-  }
-};
+// const oldDispatch = store.dispatch;
+// store.dispatch = (action) => {
+//   if (typeof action === 'function') {
+//     action(oldDispatch);
+//   } else {
+//     oldDispatch(action);
+//   }
+// };
 
 
 ReactDOM.render(
